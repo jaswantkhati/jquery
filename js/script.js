12dd8fd9d02473e7c97c21bb948fb168fe5e99b2
jaswant <jaswant@excellencetechnologies.info>
2019-06-20 12:32:15 +0530

    complte assignment 2

--- js/script.js
+++ js/script.js
@@ -10,43 +10,31 @@ $(document).ready(function() {
             $("#alert1").html("please fill this");
         } else {
             var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            var value = regEx.test(email);
-            if (value == false) {
+            if (regEx.test(email) == false) {
                 $("#alert1").html("**Enter a valid mail");
             } else {
-                $("#alert1").html("**Sucess");
+                $("#alert1").html("true");
+                $("#alert1").css('color', '#339966');
             }
         }
         if (password.length < 8 || password == '') {
             $("#alert2").html("**Enter a valid password");
         } else {
-            $("#alert2").html("**Sucess");
+            $("#alert2").html("true");
+            $("#alert2").css('color', '#339966');
         }
         if (conpassword != password || conpassword == '') {
             $("#alert3").html("**Your password is not  maching");
         } else {
-            $("#alert3").html("**Sucess");
+            $("#alert3").html("true");
+            $("#alert3").css('color', '#339966');
         }
         if ($("#check").is(":checked")) {
-            $("#alert4").html('**Sucess');
+            $("#alert4").html('true');
+            $("#alert4").css('color', '#339966');
 
         } else {
             $("#alert4").html('**Please Agree terms and conditions');
-        }
-
-        // if all cunditions are sucess
-
-        if ($("#alert1").text() == "**Sucess") {
-            $("#alert1").css('color', 'green');
-        }
-        if ($("#alert2").text() == "**Sucess") {
-            $("#alert2").css('color', 'green');
-        }
-        if ($("#alert3").text() == "**Sucess") {
-            $("#alert3").css('color', 'green');
-        }
-        if ($("#alert4").text() == "**Sucess") {
-            $("#alert4").css('color', 'green');
         }
 
     });
