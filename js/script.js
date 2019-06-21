35532624bfcb3e93d84a1b39c4b7eaee8f2db2d6
jaswant <jaswant@excellencetechnologies.info>
2019-06-21 13:26:39 +0530

    madified again

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
+    var id = 0;
     $(".add").click(function() {
-        $(".container").append('<div class="main"><input type="text"><button class="btn">Browse</button><select><option>Secondry image </option></select></div>')
-    });
-    $(".remove").click(function() {
-        $('.main').remove();
+        $(".container").append(`<div id = ${id} class="main1"><input type="text"><button class="btn">Browse</button><select><option>Secondry image </option></select><span ><button id = ${id} class="remove">remove</button></span></div>`)
+        id++;
+        let loop = id;
+        for (loop; loop > 0; loop--) {
+            $(".remove").click(function(element) {
+                $("#" + loop).remove()
+            })
+        }
     });
 });
