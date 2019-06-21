f137dd519261e0a4862d1978aa7cc085af0d3b01
jaswant <jaswant@excellencetechnologies.info>
2019-06-21 11:24:30 +0530

    modified assignmen

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
     $(".add").click(function() {
-        $(".container").append('<div class="main2"><input type="text"><button class="btn">Browse</button><select><option>Secondry image </option></select><span class="remove1">remove</span></div>')
+        $(".container").append('<div class="main"><input type="text"><button class="btn">Browse</button><select><option>Secondry image </option></select></div>')
     });
     $(".remove").click(function() {
         $('.main').remove();
-    });
-    $(".remove1").click(function() {
-        $('.main2').remove();
     });
 });
